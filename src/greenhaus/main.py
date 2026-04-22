6d0e055884d6f6a205cc63c2067f317c1d59e204
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-04-21 22:05:43 -0500

    Change greeting from 'Hello' to 'Goodbye'

--- src/greenhaus/main.py
+++ src/greenhaus/main.py
@@ -487,7 +487,7 @@ class GUI:
 			else:
 				self.water_pages[n].get_start_widget().set_label("Bed " + str(n) + " is not running.")
 		self.preview_image.set_from_file(get_file_name(attrs["last_file_number"]))
-		self.camera_text.set_label("Overall, " + str(attrs["last_file_number"]) + " images have been captured by this device.\nCurrently, images will be captured every " + str(attrs["last_file_number"]) + " seconds.")
+		self.camera_text.set_label("Overall, " + str(attrs["last_file_number"]) + " images have been captured by this device.\nCurrently, images will be captured every " + str(attrs["camera_interval"]) + " seconds.")
 		return None
 
 # Finalization and execution ****************************************************************************************
